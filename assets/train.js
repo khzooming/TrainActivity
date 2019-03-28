a61883534e3242e97def0320692e49793698a6c3
Kathryn Helsley <kathrynhelsley@Kathryns-MBP-2.frontierlocal.net>
2019-03-27 22:51:51 -0700

    HW update

---
+++ assets/train.js
@@ -0,0 +1,13 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCWMdYhUEPb2H6bwIE-SRfWMPli6-D0D58",
+    authDomain: "traintimes-31eb5.firebaseapp.com",
+    databaseURL: "https://traintimes-31eb5.firebaseio.com",
+    projectId: "traintimes-31eb5",
+    storageBucket: "traintimes-31eb5.appspot.com",
+    messagingSenderId: "1020832060874"
+  };
+  
+  firebase.initializeApp(config);
